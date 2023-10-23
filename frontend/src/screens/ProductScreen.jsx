048f0cb88d16ca5a01327524184a5b4a436e739a
ataberkakkan <akkanataberk@gmail.com>
2023-10-23 12:17:52 +0300

    Loader and Message components

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,6 +1,8 @@
 import { useParams, Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 import { useGetProductDetailsQuery } from "../slices/productSlice";
 
 const ProductScreen = () => {
@@ -18,9 +20,9 @@ const ProductScreen = () => {
         Go Back
       </Link>
       {isLoading ? (
-        <div>Loading...</div>
+        <Loader />
       ) : error ? (
-        <div>{error?.data.message || error.error}</div>
+        <Message variant="danger">{error?.data.message || error.error}</Message>
       ) : (
         <>
           <Row>
