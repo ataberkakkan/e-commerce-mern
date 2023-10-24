725989292bfb121841438c7c16d852c31749e96b
ataberkakkan <akkanataberk@gmail.com>
2023-10-24 10:23:06 +0300

    Auth Middleware and Protected Routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,11 +11,19 @@ import {
   getUserById,
   updateUser,
 } from "../controllers/userController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").get(getUsers).post(registerUser);
+router.route("/").get(protect, admin, getUsers).post(registerUser);
 router.post("/logout", logoutUser);
-router.post("/login", authUser);
-router.route("/profile").get(getUserProfile).put(updateUserProfile);
-router.route("/:id").delete(deleteUser).get(getUserById).put(updateUser);
+router.post("/auth", authUser);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
