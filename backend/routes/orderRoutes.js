40bab83c722efa7172e8af304c4843f0e1e56e23
ataberkakkan <akkanataberk@gmail.com>
2023-10-26 13:41:45 +0300

    Fixes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -12,7 +12,7 @@ import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(protect, admin, getOrders).post(protect, addOrderItems);
 router.route("/mine").get(protect, getMyOrders);
-router.route("/:id").get(protect, admin, getOrderById);
+router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, admin, updateOrderToPaid);
 router.route("/:id/deliver").put(protect, admin, updateOrderToDelivered);
 
