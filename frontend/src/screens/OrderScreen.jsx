5c33e3da141cbf8d4ce4657abe495e862fd223fe
ataberkakkan <akkanataberk@gmail.com>
2023-10-26 15:36:35 +0300

    Order Screen

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -22,8 +22,6 @@ const OrderScreen = () => {
     error,
   } = useGetOrderDetailsQuery(orderId);
 
-  console.log(order);
-
   return isLoading ? (
     <Loader />
   ) : error ? (
@@ -42,10 +40,87 @@ const OrderScreen = () => {
               <p>
                 <strong>Email: </strong> {order.user.email}
               </p>
+              <p>
+                <strong>Address: </strong>
+                {order.shippingAddress.address}, {order.shippingAddress.city}{" "}
+                {order.shippingAddress.postalCode},{" "}
+                {order.shippingAddress.country}
+              </p>
+              {order.isDelivered ? (
+                <Message variant="success">
+                  Delivered on: {order.deliveredAt}
+                </Message>
+              ) : (
+                <Message variant="danger">Not Delivered</Message>
+              )}
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Payment Method:</h2>
+              <p>
+                <strong>Method: </strong>
+                {order.paymentMethod}
+              </p>
+              {order.isPaid ? (
+                <Message variant="success">Paid on {order.paidAt}</Message>
+              ) : (
+                <Message variant="danger">Not Paid</Message>
+              )}
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Order Items</h2>
+              {order.orderItems.map((item, index) => (
+                <ListGroup.Item key={index}>
+                  <Row>
+                    <Col md={1}>
+                      <Image src={item.image} alt="item.name" fluid rounded />
+                    </Col>
+                    <Col>
+                      <Link to={`/product/${item.product}`}>{item.name}</Link>
+                    </Col>
+                    <Col md={4}>
+                      {item.qty} x ${item.price} = ${item.qty * item.price}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              ))}
             </ListGroup.Item>
           </ListGroup>
         </Col>
-        <Col md={4}></Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h2>Order Summary</h2>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Items</Col>
+                  <Col>{order.itemsPrice}</Col>
+                </Row>
+
+                <Row>
+                  <Col>Shipping</Col>
+                  <Col>{order.shippingPrice}</Col>
+                </Row>
+
+                <Row>
+                  <Col>Tax</Col>
+                  <Col>{order.taxPrice}</Col>
+                </Row>
+
+                <Row>
+                  <Col>Total</Col>
+                  <Col>{order.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              {/* Pay Order Placeholder */}
+              {/* Mark As Delivered Placeholder */}
+            </ListGroup>
+          </Card>
+        </Col>
       </Row>
     </>
   );
