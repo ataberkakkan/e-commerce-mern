d4a7aeb95511c5cb41be3c8b871399b5c0377104
ataberkakkan <akkanataberk@gmail.com>
2023-10-27 12:49:25 +0300

    Get All Orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -95,7 +95,9 @@ const updateOrderToDelivered = asyncHandler(async (req, res) => {
 });
 
 const getOrders = asyncHandler(async (req, res) => {
-  res.send("get orders");
+  const orders = await Order.find({}).populate("user", "id name");
+
+  res.status(200).json(orders);
 });
 
 export {
