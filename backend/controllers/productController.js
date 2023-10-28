0a76c0cb6d14ca7b8dee182677ec01f4632faac0
ataberkakkan <akkanataberk@gmail.com>
2023-10-28 14:54:49 +0300

    Create product backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -17,4 +17,21 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById };
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample Name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample Brand",
+    category: "Sample Category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample Description",
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createProduct);
+});
+
+export { getProducts, getProductById, createProduct };
